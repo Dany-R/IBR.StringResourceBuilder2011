40a46e3121beb4cce061930d106b964215b42c99
Dany-R <live@daniel-ruehmer.de>
2017-05-09 21:32:19 +0200

    Updated to filnal V1.5.2.20

--- IBR.StringResourceBuilder2011/IBR.StringResourceBuilder2011/Properties/AssemblyInfo.cs
+++ IBR.StringResourceBuilder2011/IBR.StringResourceBuilder2011/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("IBR")]
 [assembly: AssemblyProduct("IBR.StringResourceBuilder2011")]
-[assembly: AssemblyCopyright("© 2011-2015 by Ingenieur-Büro Rühmer")]
+[assembly: AssemblyCopyright("© 2011-2016 by Ingenieur-Büro Rühmer")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.1.19")]
-[assembly: AssemblyFileVersion("1.5.1.19")]
+[assembly: AssemblyVersion("1.5.2.20")]
+[assembly: AssemblyFileVersion("1.5.2.20")]
